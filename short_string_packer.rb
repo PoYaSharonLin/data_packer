939890365e703f5685b1d70f86562bf50cf80427
root <root@SharonsLaptop.localdomain>
2025-03-02 10:53:44 +0800

    pack: step2 bit array

--- short_string_packer.rb
+++ short_string_packer.rb
@@ -8,38 +8,39 @@ module ShortStringPacker
   # Returns: a Integer object
   def self.pack(str)
     # IMPLEMENT THIS METHOD
-    # step1: alphabet to number
+
+    # step0: prep
     alphabet = ('a'..'z').to_a
     str_array = str.chars
+
+    # step1: alphabet to number
     num_array = str_array.map { |s| alphabet.index(s) + 1 }
     # puts num_array
 
-  #   # step2: number to bits
-  #   max_bit = Math.log(26, 2).ceil
-  #     def num2bi(num)
-  #       num_array = Array.new(5)
-  #       num_array.each do |num|
-  #         lsb = num & 1
-          
-  #     end
-  #     def to binary(num_array)
-  #       num_array.each { |num| }
-  #       binary_array = []
+    # step2: number to bits
+    bit_array = Array.new(num_array.length * Math.log(26, 2).ceil)
+    bit_array.length.times do |index|
+      num_array.each do | num |
+      msb = num & 1
+      num <<= 1
+      bit_array[index] = msb
+      end 
+    end
+    puts bit_array
 
-
-  #     end
+    # step3: bits to int 
+  #   result = bit_array.reduce(0) { |acc, bit| (acc << 1) | bit }
+  #   puts result
   end
 
   ## Unpacks a Integer from pack() method into a short string
   # Arguments:
   #   packed - a Integer object
   # Returns: a String object
-  def self.unpack(packed)
-    # IMPLEMENT THIS METHOD
-  end
+  # def self.unpack(packed)
+  #   # IMPLEMENT THIS METHOD
+  # end
 end
 
-# test 
-test1 = 'string'
-puts ShortStringPacker.pack(test1)
-
+test1 = "abc"
+puts ShortStringPacker.pack(test1) 
